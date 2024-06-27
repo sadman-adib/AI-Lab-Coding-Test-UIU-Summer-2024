0042735e39714c7dc70eea69371c7791e42fcc5e
Sadman H Adib <sadmanadib5355@gmail.com>
2024-06-27 10:29:52 +0600

    test_2.py

--- test_2.py
+++ test_2.py
@@ -5,7 +5,7 @@ a = np.array([[1,2,3,4,5],
               [11,12,13,14,15],
               [16,17,18,19,20]])
 
-print(a[1:3, :]) # print 1st two rows
+print(a[1:3, :]) # print middle two rows
 print(a.sum(axis = 0)) # column wise sum
 c = a[1:2, :]
 print(c) # only 2nd row
